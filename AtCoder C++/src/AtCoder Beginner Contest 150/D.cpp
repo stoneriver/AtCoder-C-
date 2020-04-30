b88e81c755751fa7e5dbe262e32cf01efb7b4673
Ayumu Ishikawa <stoneriverau1366@gmail.com>
2020-04-30 23:20:40 +0200

    Work for 4/30/2020.

---
+++ AtCoder C++/src/AtCoder Beginner Contest 150/D.cpp
@@ -0,0 +1,12 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//	int N, M;
+//	cin >> N >> M;
+//	int a[N];
+//	for (int i = 0; i < N; i++)
+//		cin >> a[i];
+//
+//	return 0;
+//}
