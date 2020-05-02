23bd041631db5d220e52beeef7c838781510179b
Ayumu Ishikawa <stoneriverau1366@gmail.com>
2020-05-02 22:42:24 +0200

    今日のABCむずかったな 5/2/2020

--- AtCoder C++/src/AtCoder Beginner Contest 165/C.cpp
+++ AtCoder C++/src/AtCoder Beginner Contest 165/C.cpp
@@ -2,6 +2,16 @@
 //using namespace std;
 //
 //int main() {
-//
+//	int N, M, Q;
+//	cin >> N >> M >> Q;
+//	int a[Q], b[Q], c[Q], d[Q];
+//	for (int i = 0; i < Q; i++)
+//		cin >> a[i] >> b[i] >> c[i] >> d[i];
+//	vector<int> A(pow(M, N));
+//	for (int i = 0; i < Q; i++) {
+//		int dif = b[i] - a[i];
+//		for (int j = 0; j + dif < M; j++) {
+//		}
+//	}
 //	return 0;
 //}
